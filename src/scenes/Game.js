f9ed8852fa0667f0c579656d000e054e7b56ed57
LiivShipper <livia.borba11@gmail.com>
2025-04-28 23:17:32 -0300

    Animação e camera adicionados

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -3,47 +3,108 @@ import { Scene } from 'phaser';
 export class Game extends Scene {
     constructor() {
         super('Game');
+        this.player = null;
+        this.cursors = null;
     }
 
     preload() {
         this.load.image('tiles', 'assets/tilesets.png');
         this.load.tilemapTiledJSON('mapa', 'assets/sustainaQuest.json');
-        //this.load.image('player', 'assets/player.png');
+        this.load.spritesheet('player', 'assets/player.png', {
+            frameWidth: 32,
+            frameHeight: 64,
+            spacing: 0,
+            margin: 0
+        });
     }
 
     create() {
         const mapa = this.make.tilemap({ key: 'mapa' });
         const elementos = mapa.addTilesetImage('tilesets', 'tiles');
 
-        const camadaGrama = mapa.createLayer('grama', elementos, 0, 0);
-        const camadaAgua = mapa.createLayer('agua', elementos, 0, 0);
-        const camadaVila = mapa.createLayer('vila', elementos, 0, 0);
-        const camadaEnergiaEolica = mapa.createLayer('energiaEolica', elementos, 0, 0);
-        const camadaEnergiaHidreletrica = mapa.createLayer('energiaHidreletrica', elementos, 0, 0);
-        const camadaEnergiaMaremotriz = mapa.createLayer('energiaMaremotriz', elementos, 0, 0);
-        const camadaEnergiaBiomassa = mapa.createLayer('energiaBiomassa', elementos, 0, 0);
-        const camadaEnergiaHidrogenio = mapa.createLayer('energiaHidrogenio', elementos, 0, 0);
-        const camadaEnergiaSolar = mapa.createLayer('energiaSolar', elementos, 0, 0);
-        const camadaEnergiaGeotermica = mapa.createLayer('energiaGeotermica', elementos, 0, 0);
+        mapa.createLayer('grama', elementos, 0, 0);
+        mapa.createLayer('agua', elementos, 0, 0);
+        mapa.createLayer('vila', elementos, 0, 0);
+        mapa.createLayer('energiaEolica', elementos, 0, 0);
+        mapa.createLayer('energiaHidreletrica', elementos, 0, 0);
+        mapa.createLayer('energiaMaremotriz', elementos, 0, 0);
+        mapa.createLayer('energiaBiomassa', elementos, 0, 0);
+        mapa.createLayer('energiaHidrogenio', elementos, 0, 0);
+        mapa.createLayer('energiaSolar', elementos, 0, 0);
+        mapa.createLayer('energiaGeotermica', elementos, 0, 0);
 
-        this.cameras.main.setZoom(0.8);
-        this.cameras.main.setBounds(0, 0, mapa.widthInPixels, mapa.heightInPixels);
+        this.player = this.physics.add.sprite(70, 810, "player");
+        this.player.setScale(1.5);
+        this.cursors = this.input.keyboard.createCursorKeys();
 
+        this.anims.create({
+            key: 'frente',
+            frames: this.anims.generateFrameNumbers('player', { start: 8, end: 9 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'tras',
+            frames: this.anims.generateFrameNumbers('player', { start: 18, end: 19 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'direita',
+            frames: this.anims.generateFrameNumbers('player', { start: 28, end: 29 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'esquerda',
+            frames: this.anims.generateFrameNumbers('player', { start: 30, end: 31 }),
+            frameRate: 10,
+            repeat: -1
+        });
 
-        //player = this.physics.add.sprite(100, 300, "player");
+        const camera = this.cameras.main;
+        camera.setBounds(0, 0, mapa.widthInPixels, mapa.heightInPixels);
+        camera.startFollow(this.player);
+        camera.setZoom(2);
     }
 
-    /*update() {
-        cursors = this.input.keyboard.createCursorKeys()
+    update() {
+        if (!this.cursors || !this.player) return;
 
-        if(cursors.left.isDown) {
-            player.body.setVelocityX(-100)
-        } else if (cursors.right.isDown) {
-            player.body.setVelocityX(100)
-        } else if (cursors.up.isDown) {
-            player.body.setVelocityY(-100) 
-        } else if (cursors.down.isDown) {
-            player.body.setVelocityY(100)
+        const prevVelocity = this.player.body.velocity.clone();
+        this.player.body.setVelocity(0);
+
+        // Movimentação
+        if (this.cursors.left.isDown) {
+            this.player.body.setVelocityX(-150);
+            this.player.anims.play('esquerda', true);
+            this.player.setFlipX(false);
         }
-    }*/
+        else if (this.cursors.right.isDown) {
+            this.player.body.setVelocityX(150);
+            this.player.anims.play('direita', true);
+            this.player.setFlipX(false);
+        }
+        else if (this.cursors.up.isDown) {
+            this.player.body.setVelocityY(-150);
+            this.player.anims.play('tras', true);
+        }
+        else if (this.cursors.down.isDown) {
+            this.player.body.setVelocityY(150);
+            this.player.anims.play('frente', true);
+        }
+        else {
+            this.player.anims.stop();
+
+            if (prevVelocity.x < 0) {
+                this.player.setFrame(30); // parado virado para esquerda
+            } else if (prevVelocity.x > 0) {
+                this.player.setFrame(20); // parado virado para direita
+            } else if (prevVelocity.y < 0) {
+                this.player.setFrame(10); // parado virado para cima
+            } else if (prevVelocity.y > 0) {
+                this.player.setFrame(0);  // parado virado para baixo
+            }
+        }
+    }
 }
